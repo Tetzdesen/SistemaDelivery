76cfb659f736965cd4a4a57d4ecedfab72ef1a25
Tetzdesen <Tetzdesen@gmail.com>
2024-11-15 03:08:25 -0300

    Change request

--- src/main/java/ufes/br/desconto/MetodoDescontoPorDataPedido.java
+++ src/main/java/ufes/br/desconto/MetodoDescontoPorDataPedido.java
@@ -18,15 +18,15 @@ public class MetodoDescontoPorDataPedido implements IMetodoDescontoTaxaEntrega {
     private final Map<LocalDate,Double> descontosPorData = new HashMap<>();
     
     public MetodoDescontoPorDataPedido(){   
-        descontosPorData.put(LocalDate.of(2025, Month.JANUARY, 1), 1.00);
-        descontosPorData.put(LocalDate.of(2024, Month.OCTOBER, 12), 1.50);
-        descontosPorData.put(LocalDate.of(2024, Month.DECEMBER, 25), 2.00);
+        descontosPorData.put(LocalDate.of(2025, Month.JANUARY, 1), 0.10);
+        descontosPorData.put(LocalDate.of(2024, Month.OCTOBER, 12), 0.15);
+        descontosPorData.put(LocalDate.of(2024, Month.DECEMBER, 25), 0.20);
     }
     
     @Override
     public void calcularDesconto(Pedido pedido) {
         if(seAplica(pedido)){
-            pedido.aplicarDesconto(new CupomDescontoEntrega("Desconto pela data do pedido", descontosPorData.get(LocalDate.now())));
+            pedido.aplicarDesconto(new CupomDescontoEntrega("Desconto pela data do pedido", descontosPorData.get(pedido.getDataPedido())));
         }
     }
 
@@ -34,15 +34,6 @@ public class MetodoDescontoPorDataPedido implements IMetodoDescontoTaxaEntrega {
     public boolean seAplica(Pedido pedido) {
       LocalDate dataAtualPedido = pedido.getDataPedido();
       return descontosPorData.containsKey(dataAtualPedido);
-    //  int mes = dataAtualPedido.getMonthValue();
-      //int ano = dataAtualPedido.getYear();
-      
-      /*
-      for (Map.Entry<LocalDate, Double> descontoData : descontosPorData.entrySet()) {
-          LocalDate data = descontoData.getKey();
-          return mes == descontosPorData.
-      }*/
-
     }
     
 }
